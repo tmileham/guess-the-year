7f1aaf8f4b6de8eeb8cc1b777a7c096b71b7defa
Thomas Mileham <Thomas.Mileham@fos.net>
2022-03-13 22:17:41 +0000

    Func to retrieve movie data from API

---
+++ APP-PLAN.md
@@ -0,0 +1,9 @@
+Using the IMDB ID retrieve the title and name of the movie.
+
+User can then have a guess of the year
+
+If correct, user gains a point, next question.
+
+If Incorrect, user loses a life
+
+######################
--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import API_KEY from "./API_KEY";
+import MovieCard from "./components/MovieCard";
 
 const App = () => {
+  // Example Movie, for now.
+  const TEMP_EXAMPLE_MOVIE_ID = "tt0114709";
+
+  //States
+  const [movies, setMovies] = useState([]);
+
+  // useEffect hook run on app launch, for now.
+  useEffect(() => {
+    searchMovie(TEMP_EXAMPLE_MOVIE_ID);
+  }, []);
+
+  // Retrieve Data from API
+  const searchMovie = async (id) => {
+    const response = await fetch(`${API_KEY + id}`);
+    const data = response.json();
+    setMovies(data);
+    console.log(movies);
+  };
+
   return (
     <div className="App">
-      <h1 className='title'>Guess the year</h1>
+      <h1 className="title">Guess the year</h1>
+      <MovieCard />
     </div>
   );
-}
+};
 
 export default App;

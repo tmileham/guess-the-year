ca382d8cb6d28701883dcfe906d2968f9b92c7dc
Tom Mileham <tom.mileham@hotmail.co.uk>
2022-03-23 21:39:20 +0000

    Setup configure quiz API

--- src/ConfigureQuiz.js
+++ src/ConfigureQuiz.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import "./ConfigureQuiz.css";
+
 //components
 
 import MovieResult from "./components/configure/MovieResult";
@@ -9,21 +11,30 @@ const API_URL = "http://www.omdbapi.com/?apikey=42041cf9";
 const ConfigureQuiz = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState([]);
+  const [selectedMovies, setSelectedMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("as");
-  }, []);
+    searchMovies(searchTerm);
+  }, [searchTerm]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
+    console.log(data.Search);
   };
 
   return (
-    <div>
+    <div className="configureContainer">
       <h1>Setup / Configure Quiz</h1>
+      <p>Search and select movies for the quiz</p>
+      <div className="SelectedItems">
+        <p>Selected Movies:</p>
+        <p>No movies selected</p>
+        <button>Save</button>
+      </div>
+
       <div className="search">
         <input
           placeholder="Search for a movie"
@@ -40,12 +51,13 @@ const ConfigureQuiz = () => {
           }}
         />
       </div>
-
       {movies?.length > 0 ? (
         <div className="container">
-          {movies.map((movie) => (
-            <MovieResult movie={movie} />
-          ))}
+          {movies
+            .filter((movie) => movie.Type === "movie" && movie.Poster !== "N/A")
+            .map((movie) => (
+              <MovieResult movie={movie} />
+            ))}
         </div>
       ) : (
         <div className="empty">
--- src/components/configure/MovieResult.js
+++ src/components/configure/MovieResult.js
@@ -3,9 +3,7 @@ import React from "react";
 const MovieCard = ({ movie }) => {
   return (
     <div className="movie">
-      <div>
-        <p>{movie.Year}</p>
-      </div>
+      <div>{/* <p>{movie.Year}</p> */}</div>
 
       <div>
         <img
